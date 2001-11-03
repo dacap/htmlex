6dde9a5113d86b7f7a6519052f83ead4ebf80feb
David Capello <davidcapello@gmail.com>
2001-11-03 03:50:33 +0000

    Removed the old files of the htmlex perl script

--- examples/all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# test all examples
-
-for file in *.htex ; do
-  clear ; echo "This code from \`$file'..." ; cat "$file"
-  read -p "Press any key to continue..." -n 1
-  clear ; echo "Produce this results..." ; ../htmlex "$file"
-  read -p "Press any key to continue..." -n 1
-done
